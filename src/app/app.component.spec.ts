fae960af6cc31209147d21274544cc981d524126
Sahil Negi <sahilnegi941@gmail.com>
2020-09-17 14:01:17 +0530

    Final Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'TO-DO'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TO-DO');
+    expect(app.name).toEqual('TO-DO');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TO-DO';
+export class AppComponent  {
+  name = 'Sahil';
+
+  a: number = 0.259;
+  
+  birthday = new Date(1997, 9, 4); 
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
 }
